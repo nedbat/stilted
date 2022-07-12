431450567727cce0225a5a62eeeac444c616fbec
Ned Batchelder <ned@nedbatchelder.com>
2022-07-12 14:07:24 -0400

    simplify lexer: no kind, just typed values

--- lex.py
+++ lex.py
@@ -10,8 +10,8 @@ class Token:
     """A token that we want."""
 
     rx: str
-    kind: str
     converter: Callable[[str], Any] = lambda text: text
+    keep: bool = True
 
     def pattern(self):
         return f"(?P<{self.kind}>{self.rx})"
@@ -22,7 +22,7 @@ class Skip:
     """Characters that can be discarded."""
 
     rx: str
-    kind: None = None
+    keep: bool = False
 
     def pattern(self):
         return f"({self.rx})"
@@ -36,17 +36,25 @@ class Lexer:
     """
 
     def __init__(self, *tokens) -> None:
-        self.rx = "(?m)" + "|".join(t.pattern() for t in tokens)
-        self.converters = {t.kind: t.converter for t in tokens if t.kind}
+        rxes = []
+        self.converters = {}
+        for i, t in enumerate(tokens):
+            if t.keep:
+                group_name = f"g{i}"
+                rxes.append(f"(?P<{group_name}>{t.rx})")
+                self.converters[group_name] = t.converter
+            else:
+                rxes.append(f"({t.rx})")
+        self.rx = "(?m)" + "|".join(rxes)
 
     def tokens(self, text: str) -> Iterable[Tuple[str, Any]]:
         """
         Yield (kind, value) pairs for the tokens in `text`.
         """
         for match in re.finditer(self.rx, text):
-            if kind := match.lastgroup:
-                converter = self.converters[kind]
-                yield (kind, converter(match[0]))
+            if group_name := match.lastgroup:
+                converter = self.converters[group_name]
+                yield converter(match[0])
 
 
 def convert_string(text: str) -> str:
@@ -73,12 +81,14 @@ def convert_string(text: str) -> str:
 
     return re.sub(r"(?s)\\[0-7]{1,3}|\\.", do_escape, text[1:-1])
 
+def error(text: str):
+    raise Exception(f"Lexical error: {text!r}")
 
 lexer = Lexer(
-    Token(r"\((?:\\\[0-7]{1,3}|\\.|\\\n|.|\n)*?\)", "string", convert_string),
-    Token(r"[-+]?\d*(\d\.|\.\d)\d*", "float", float),
-    Token(r"[-+]?\d+", "int", int),
+    Token(r"\((?:\\\[0-7]{1,3}|\\.|\\\n|.|\n)*?\)", convert_string),
+    Token(r"[-+]?\d*(\d\.|\.\d)\d*", float),
+    Token(r"[-+]?\d+", int),
     Skip(r"%.*$"),
     Skip(r"\s+"),
-    Token(r".", "error"),
+    Token(r".", error),
 )
