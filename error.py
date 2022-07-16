10411d9d08e9925dd05944eec316dbb81659c941
Ned Batchelder <ned@nedbatchelder.com>
2022-07-16 07:14:38 -0400

    clean up Tilted

--- error.py
+++ error.py
@@ -1,5 +1,8 @@
 """Errors that stilted can raise."""
 
 class Tilted(Exception):
+    """Any stilted exception."""
+
     def __init__(self, errname):
+        super().__init__(errname)
         self.errname = errname
