d12a03f347030913cd04fdabee604edd18275b05
Ned Batchelder <ned@nedbatchelder.com>
2022-08-06 07:45:22 -0400

    rotate, scale.  100 operators

--- op_matrix.py
+++ op_matrix.py
@@ -1,5 +1,7 @@
 """Built-in matrix and coordinate operators for stilted."""
 
+import math
+
 import cairo
 
 from dtypes import from_py, typecheck, Array, Real, Integer, Number
@@ -18,6 +20,10 @@ def matrix_to_array(mtx: cairo.Matrix, arr: Array):
     lmtx = list(mtx) # type: ignore
     for i in range(6):
         arr[i] = from_py(lmtx[i])
+
+def deg_to_rad(degrees: float) -> float:
+    """Convert degrees to radians."""
+    return math.pi * degrees / 180
 
 @operator
 def currentmatrix(estate: ExecState) -> None:
@@ -38,10 +44,44 @@ def matrix(estate: ExecState) -> None:
     estate.opush(arr)
 
 @operator
+def rotate(estate: ExecState) -> None:
+    match estate.otop():
+        case Array():
+            arr = pop_matrix(estate)
+            ang = estate.opop(Number)
+            matrix_to_array(cairo.Matrix.init_rotate(deg_to_rad(ang.value)), arr)
+            estate.opush(arr)
+
+        case Integer() | Real():
+            ang = estate.opop(Number)
+            estate.gctx.rotate(deg_to_rad(ang.value))
+
+        case _:
+            raise Tilted("typecheck")
+
+@operator
+def scale(estate: ExecState) -> None:
+    match estate.otop():
+        case Array():
+            arr = pop_matrix(estate)
+            sx, sy = estate.opopn(2)
+            typecheck(Number, sx, sy)
+            matrix_to_array(cairo.Matrix(xx=sx.value, yy=sy.value), arr)
+            estate.opush(arr)
+
+        case Integer() | Real():
+            sx, sy = estate.opopn(2)
+            typecheck(Number, sx, sy)
+            estate.gctx.scale(sx.value, sy.value)
+
+        case _:
+            raise Tilted("typecheck")
+
+@operator
 def translate(estate: ExecState) -> None:
     match estate.otop():
         case Array():
-            arr = estate.opop()
+            arr = pop_matrix(estate)
             tx, ty = estate.opopn(2)
             typecheck(Number, tx, ty)
             matrix_to_array(cairo.Matrix(x0=tx.value, y0=ty.value), arr)
--- test_op_matrix.py
+++ test_op_matrix.py
@@ -18,6 +18,11 @@ from test_helpers import compare_stacks
         ("6 array identmatrix aload pop", [1.0, 0.0, 0.0, 1.0, 0.0, 0.0]),
         # matrix
         ("matrix aload pop", [1.0, 0.0, 0.0, 1.0, 0.0, 0.0]),
+        # rotate
+        ("30 matrix rotate 1 get 0.5 sub abs 0.000001 lt", [True]),
+        ("30 matrix rotate 2 get -0.5 sub abs 0.000001 lt", [True]),
+        # scale
+        ("17 42 matrix scale aload pop", [17.0, 0.0, 0.0, 42.0, 0.0, 0.0]),
         # translate
         ("17 42 translate matrix currentmatrix dup 4 get exch 5 get", [17.0, 42.0]),
         ("17 42 matrix translate aload pop", [1.0, 0.0, 0.0, 1.0, 17.0, 42.0]),
@@ -39,6 +44,19 @@ def test_evaluate(text, stack):
         ("identmatrix", "stackunderflow"),
         ("123 identmatrix", "typecheck"),
         ("5 array identmatrix", "rangecheck"),
+        # rotate
+        ("rotate", "stackunderflow"),
+        ("(a) rotate", "typecheck"),
+        ("(a) matrix rotate", "typecheck"),
+        ("1 [1] rotate", "rangecheck"),
+        # scale
+        ("scale", "stackunderflow"),
+        ("1 scale", "stackunderflow"),
+        ("(a) 1 scale", "typecheck"),
+        ("1 (a) scale", "typecheck"),
+        ("1 (a) matrix scale", "typecheck"),
+        ("(a) 1 matrix scale", "typecheck"),
+        ("1 1 [1] scale", "rangecheck"),
         # translate
         ("translate", "stackunderflow"),
         ("1 translate", "stackunderflow"),
@@ -46,6 +64,7 @@ def test_evaluate(text, stack):
         ("1 (a) translate", "typecheck"),
         ("1 (a) matrix translate", "typecheck"),
         ("(a) 1 matrix translate", "typecheck"),
+        ("1 1 [1] translate", "rangecheck"),
     ],
 )
 def test_evaluate_error(text, error):
