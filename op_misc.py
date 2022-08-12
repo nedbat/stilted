76b408463aee6b38c4b65b20b27d27f4c91fd313
Ned Batchelder <ned@nedbatchelder.com>
2022-08-12 17:28:01 -0400

    bind

--- op_misc.py
+++ op_misc.py
@@ -2,9 +2,27 @@
 
 import time
 
-from dtypes import from_py
+from dtypes import from_py, Array, Name, Operator
 from evaluate import operator, Engine
 
+@operator
+def bind(engine: Engine) -> None:
+
+    def bind_proc(proc: Array) -> None:
+        """Recursive function to replace executable names with operators."""
+        for i, elt in enumerate(proc):
+            match elt:
+                case Array():
+                    bind_proc(elt)
+
+                case Name(literal=False):
+                    val = engine.dstack_value(elt)
+                    if isinstance(val, Operator):
+                        proc[i] = val
+
+    proc = engine.opop(Array)
+    bind_proc(proc)
+    engine.opush(proc)
 
 @operator
 def usertime(engine: Engine) -> None:
--- test_op_misc.py
+++ test_op_misc.py
@@ -3,6 +3,7 @@
 import pytest
 
 from dtypes import Name
+from error import StiltedError
 from evaluate import evaluate
 from test_helpers import compare_stacks
 
@@ -10,9 +11,28 @@ from test_helpers import compare_stacks
 @pytest.mark.parametrize(
     "text, stack",
     [
+        # bind
+        ("[] bind", "[]"),
+        ("{1 2 add {mul} 3} bind", "[ 1 2 /add load [ /mul load ] cvx 3] cvx"),
+        ("{1 2 /add {/mul} 3} bind", "[ 1 2 /add [ /mul ] cvx 3] cvx"),
+        ("[1 2 /add cvx [/mul cvx] 3] bind", "[ 1 2 /add load [ /mul load ] 3]"),
+        ("{ xyzzy product } bind", "{ xyzzy product }"),
         # usertime
         ("usertime type", [Name(False, "integertype")]),
     ],
 )
 def test_evaluate(text, stack):
     compare_stacks(evaluate(text).ostack, stack)
+
+
+@pytest.mark.parametrize(
+    "text, error",
+    [
+        # bind
+        ("bind", "stackunderflow"),
+        ("1 bind", "typecheck"),
+    ],
+)
+def test_evaluate_error(text, error):
+    with pytest.raises(StiltedError, match=error):
+        evaluate(text)
